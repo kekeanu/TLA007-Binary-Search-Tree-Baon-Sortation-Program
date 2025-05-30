2d9874ac429367ed24f2fa3dbe9a2b737c7a247e
kekeanu <klumando_240000000910@uic.edu.ph>
2025-05-30 15:08:08 +0800

    Add files via upload

---
+++ BinaryTree/src/main/java/com/mycompany/binarytree/BinaryTree.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.binarytree;
+
+/**
+ *
+ * @author Student's Account
+ */
+public class BinaryTree {
+
+    public static void main(String[] args) {
+       Node root;
+    
+    public void insert(int value){
+        root = insertRec(root,value);
+    }
+    private Node insertRec(Node root, int value){
+        if(root == null){
+            return new Node(value);
+        }
+        if (value < root.value){
+            root.left=insertRec(root.left, value);
+        } else if (value > root.value){
+            root.right = insertRec(root.right, value);
+        }
+      return root;
+    }
+    public void inorder(){inorderRec (root);}
+    
+    
+    public void inorderRec(Node root){
+        if(root !=null){
+            inorderRec(root.left);
+            System.out.println(root.value+"");
+            inorderRec(root.right);
+            
+        }  
+    }
+}
+    }
+}
